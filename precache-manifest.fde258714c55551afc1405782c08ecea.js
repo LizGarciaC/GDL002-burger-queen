05c3134d465fe8346bacf1eab14c1a61573b4cb5
LizGarciaC <lunaobel@hotmail.com>
2019-05-28 09:31:08 -0500

    Updates

---
+++ precache-manifest.fde258714c55551afc1405782c08ecea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60541e8223b4f24a7722da6bccaf81a",
+    "url": "/GDL002-burger-queen/index.html"
+  },
+  {
+    "revision": "f159022a1c128522b69e",
+    "url": "/GDL002-burger-queen/static/css/main.88fb5b92.chunk.css"
+  },
+  {
+    "revision": "7d8e144ec2c17aa97555",
+    "url": "/GDL002-burger-queen/static/js/2.b5f2e619.chunk.js"
+  },
+  {
+    "revision": "f159022a1c128522b69e",
+    "url": "/GDL002-burger-queen/static/js/main.1b3c3fc1.chunk.js"
+  },
+  {
+    "revision": "f8c6079179fe30af807c",
+    "url": "/GDL002-burger-queen/static/js/runtime~main.2601196f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12af2c4ea0b21524c729174f456344db.js"
+  "/GDL002-burger-queen/precache-manifest.fde258714c55551afc1405782c08ecea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GDL002-burger-queen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
