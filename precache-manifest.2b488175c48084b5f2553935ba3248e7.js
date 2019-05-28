d4f66769275617a00d0cd86863ca0f1081aadb86
LizGarciaC <lunaobel@hotmail.com>
2019-05-28 09:09:48 -0500

    Updates

---
+++ precache-manifest.2b488175c48084b5f2553935ba3248e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464bd4e5226a99e68e56b2d0769162f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abff39642aba63d6e0b8",
+    "url": "/static/css/main.88fb5b92.chunk.css"
+  },
+  {
+    "revision": "dcd7d3329a89a2a593aa",
+    "url": "/static/js/2.8f29c0d1.chunk.js"
+  },
+  {
+    "revision": "abff39642aba63d6e0b8",
+    "url": "/static/js/main.adba14fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.2b488175c48084b5f2553935ba3248e7.js"
 );
 
 self.addEventListener('message', (event) => {
